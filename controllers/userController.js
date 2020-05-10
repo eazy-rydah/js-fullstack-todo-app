a0787257758a2e07fb2a24b8cf62750623571353
Fabian <fabian.rhoda@gmx.net>
2020-05-10 21:07:53 +0200

    [feature] Email/Username existcheck registration

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,45 +1,47 @@
-const User = require("../models/User.js");
+const User = require('../models/User')
 
-exports.login = (req, res) => {
-    let user = new User(req.body);
-    user.login().then((result) => {
-      req.session.user = {favColor: "blue", username: user.data.username}
-      req.session.save(() => {
-        res.redirect("/");
-      })
-    }).catch((err) => {
-      req.flash("errors", err);
-      req.session.save(() => {
-        res.redirect("/");
-      });
-    });
+exports.login = function(req, res) {
+  let user = new User(req.body)
+  user.login().then(function(result) {
+    req.session.user = {favColor: "blue", username: user.data.username}
+    req.session.save(function() {
+      res.redirect('/')
+    })
+  }).catch(function(e) {
+    req.flash('errors', e)
+    req.session.save(function() {
+      res.redirect('/')
+    })
+  })
 }
 
-exports.logout = (req, res) => {
-    req.session.destroy(() => {
-      res.redirect("/");
-    });
+exports.logout = function(req, res) {
+  req.session.destroy(function() {
+    res.redirect('/')
+  })
 }
 
-exports.register = (req, res) => {
-  let user = new User(req.body);
-  user.register();
-  if (user.errors.length) {
-    user.errors.forEach((error) => {
-      req.flash("regErrors", error);
-    });
-    req.session.save(() => {
-      res.redirect("/");
-    });
-  } else {
-    res.send("Congrats there are no errors");
-  }
+exports.register = function(req, res) {
+  let user = new User(req.body)
+  user.register().then(() => {
+    req.session.user = {username: user.data.username}
+    req.session.save(function() {
+      res.redirect('/')
+    })
+  }).catch((regErrors) => {
+    regErrors.forEach(function(error) {
+      req.flash('regErrors', error)
+    })
+    req.session.save(function() {
+      res.redirect('/')
+    })
+  })
 }
 
-exports.home = (req, res) => {
+exports.home = function(req, res) {
   if (req.session.user) {
-    res.render("home-dashboard.ejs", {username: req.session.user.username});
+    res.render('home-dashboard', {username: req.session.user.username})
   } else {
-    res.render("home-guest.ejs", {errors: req.flash("errors"), regErrors: req.flash("regErrors")});
+    res.render('home-guest', {errors: req.flash('errors'), regErrors: req.flash('regErrors')})
   }
 }
